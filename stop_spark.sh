4486996b785c2cec7645dcc6539825ed4876ca18
Pasquale Andreacchio <pasquale.andreacchio@studenti.unipd.it>
2026-09-03 15:53:35 +0000

    feat: added conversion of the A5 and A9 metrics

--- stop_spark.sh
+++ stop_spark.sh
@@ -6,5 +6,13 @@ cd $SPARK_HOME
 rm -rf /usr/local/spark/work/app-*
 sudo rm -rf /opt/spark-tmp/*
 
+for slave in slave-1 slave-2 slave-3; do
+  echo "Emptying $slave..."
+  ssh $slave "rm -rf /usr/local/spark/work/*"          # old Spark jobs
+  ssh $slave "rm -rf /usr/local/spark/logs/*.out"      # Spark logs
+  ssh $slave "rm -rf ~/.ivy2.5.2/jars/* ~/.ivy2.5.2/cache/*"   # cache JAR Ivy 
+  ssh $slave "sudo rm -rf /tmp/spark-*"                # tmp Spark
+done
+
 # In caso ci siano problemi di connessione rifiutata eseguire il comando sotto
 # pkill -f "spark.deploy"
