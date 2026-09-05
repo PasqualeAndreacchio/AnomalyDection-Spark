bbd9aafac9f97cdbb61c6e53a454597eeaadc82c
Pasquale Andreacchio <pasquale.andreacchio@studenti.unipd.it>
2026-09-05 10:48:56 +0000

    added "timestamp_bucket" for normalizing the sampling frequence of data

--- stop_spark.sh
+++ stop_spark.sh
@@ -9,7 +9,6 @@ sudo rm -rf /opt/spark-tmp/*
 for slave in slave-1 slave-2 slave-3; do
   echo "Emptying $slave..."
   ssh $slave "rm -rf /usr/local/spark/work/*"          # old Spark jobs
-  ssh $slave "rm -rf /usr/local/spark/logs/*.out"      # Spark logs
   ssh $slave "rm -rf ~/.ivy2.5.2/jars/* ~/.ivy2.5.2/cache/*"   # cache JAR Ivy 
   ssh $slave "sudo rm -rf /tmp/spark-*"                # tmp Spark
 done
